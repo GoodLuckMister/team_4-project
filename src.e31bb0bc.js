76cb28a6545bb051ed157aaec500ea73c8013722
GodLuckMister <paet14@gmail.com>
2020-12-18 19:46:46 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -189,10 +189,17 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./..\\images\\slider-test\\Vector.svg":[["Vector.0eb086f5.svg","images/slider-test/Vector.svg"],"images/slider-test/Vector.svg"],"./..\\images\\slider-test\\devil.svg":[["devil.293b3778.svg","images/slider-test/devil.svg"],"images/slider-test/devil.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./sass/main.scss");
+
+new Swiper('.image-slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
 },{"./sass/main.scss":"sass/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -221,7 +228,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57084" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62439" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
