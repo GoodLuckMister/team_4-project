3375c3a32fe935484c640f7b984cf2d650864132
GodLuckMister <paet14@gmail.com>
2020-12-20 23:53:04 +0200

    add header

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const mySwiper = new Swiper('.image-slider', {
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const buttonHidden = document.querySelector("[button-hidden]");
+  const headerContPadding = document.querySelector("[padding]")
 
 
   menuBtnRef.addEventListener("click", () => {
@@ -21,5 +23,7 @@ const mySwiper = new Swiper('.image-slider', {
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+    buttonHidden.classList.toggle("is-hidden");
+    headerContPadding.classList.toggle("padding-zero")
   });
 })();
